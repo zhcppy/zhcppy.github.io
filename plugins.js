3ac45f591dfd2a650ea8b0b403fb9a8e20c5e0dc
zhanghang <zhcppy@icloud.com>
2020-01-31 16:49:06 +0800

    记住：你是你生命的船长；走自己的路，何必在乎其它。

--- plugins.js
+++ plugins.js
@@ -1,21 +1,21 @@
 window.$docsify.plugins = [
     function (hook, vm) {
+        // 页头
         hook.beforeEach(function (content) {
             let url = 'https://github.com/zhcppy/zhcppy.github.io/blob/master/' + vm.route.file;
             let editHtml = '[:memo: Edit Document](' + url + ')\n';
             return editHtml + content + '\n----\n' +'Last modified {docsify-updated}';
-        })
-    },
-    function (hook) {
-        let footer = [
-            '<footer>',
-            '<span>&copy;2019-2020 <a href="https://github.com/zhcppy">zhcppy</a> Copyright. </span>',
-            '<span>Powered by <a href="https://github.com/docsifyjs/docsify" target="_blank">docsify</a>.</span>',
-            '</footer>'
-        ].join('');
+        });
 
+        // 页尾
         hook.afterEach(function (html) {
+            let footer = [
+                '<footer>',
+                '<span>&copy;2019-2020 <a href="https://github.com/zhcppy">zhcppy</a> Copyright. </span>',
+                '<span>Powered by <a href="https://github.com/docsifyjs/docsify" target="_blank">docsify</a>.</span>',
+                '</footer>'
+            ].join('');
             return html + footer;
         });
-    }
+    },
 ];
